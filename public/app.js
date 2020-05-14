8a74745f23eff3d4382bfa1492e9497462c1d0bb
mvmmarcus <marcus.vinicius.marques@hotmail.com>
2020-05-14 20:38:00 -0300

    Add contact part

--- public/app.js
+++ public/app.js
@@ -1,6 +1,8 @@
-const menuItems = document.querySelectorAll(".header__menu a[href^='#']");
-
+const menuItems = document.querySelectorAll(
+  "a[href^='#']" || "div[id^='m']"
+);
 menuItems.forEach((item) => {
+  console.log(item)
   item.addEventListener("click", scrollToIdOnClick);
 });
 
@@ -9,9 +11,18 @@ function scrollToIdOnClick(event) {
   const element = event.target;
   const id = element.getAttribute("href");
   const to = document.querySelector(id).offsetTop;
-
   window.scroll({
     top: to - 60,
     behavior: "smooth",
   });
 }
+
+const btnTop = document.getElementById("moveToTop");
+
+window.onscroll = () => {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    btnTop.classList.add("moveToTop--visible");
+  } else {
+    btnTop.classList.remove("moveToTop--visible");
+  }
+};
